8b9f40f14e00a2732c8eb2003d49ee9b58af66d8
Mehmet Samet <msametgok@hotmail.com>
2021-09-05 20:06:43 +0300

    mongodb database connection and post photo model to database

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const path = require('path');
 const ejs = require('ejs');
+
+const Photo = require('./models/Photo');
+
 const app = express();
+
+// Create and connect Database
+mongoose.connect('mongodb://localhost/pcat-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -20,6 +33,11 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
     res.render('add');
 });
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
+})
 
 const port = 3000;
 app.listen(port, () => {
