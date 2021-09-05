32983ed1aa6c54c44c590c3ee0685dc5ff401c00
Mehmet Samet <msametgok@hotmail.com>
2021-09-06 00:37:03 +0300

    Photo view added and routed from index

--- app.js
+++ app.js
@@ -19,25 +19,32 @@ app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // ROUTES
 app.get('/', async (req, res) => {
   const photos = await Photo.find({});
-  res.render('index', {photos});
+  res.render('index', { photos });
 });
+
+app.get('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', { photo });
+});
+
 app.get('/about', (req, res) => {
-    res.render('about');
+  res.render('about');
 });
+
 app.get('/add', (req, res) => {
-    res.render('add');
+  res.render('add');
 });
 
 app.post('/photos', async (req, res) => {
   await Photo.create(req.body);
   res.redirect('/');
-})
+});
 
 const port = 3000;
 app.listen(port, () => {
